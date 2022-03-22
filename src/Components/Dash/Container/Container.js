93474c46f027f4d111c077985676c91986d5b92a
MichaelWhitt <michaelwhitt90@gmail.com>
2022-03-22 17:10:12 -0500

    feat(columns): add ability to add new columns

--- src/Components/Dash/Container/Container.js
+++ src/Components/Dash/Container/Container.js
@@ -4,11 +4,22 @@ import CardColumn from "./Card/CardColumn"
 
 export class Container extends React.Component {
 
+    state={
+        columns: [<CardColumn />]
+    }
+
+    createNewColumn = () => {
+        this.setState({columns: [...this.state.columns, <CardColumn />]})
+    }
+
 render () {
     return(
         <div className={`${styles.container}`}>
             <div style={{display: 'flex'}}>
-                <CardColumn />
+                {this.state.columns.map(c => c)}
+                <div>
+                    <button className={`${styles.newListBtn}`} onClick={this.createNewColumn}>+ Create New List</button>
+                </div>    
             </div>
         </div>
     )
